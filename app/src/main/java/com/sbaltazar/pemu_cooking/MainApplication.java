f2fdc421e8b90b6b2abc1aa4e93ea25508efcfeb
Sebastian Baltazar <sebastian.baltazar@antaramining.cl>
2019-05-27 21:49:19 -0400

    chore: When the app is in DEBUG mode, timber logging is enabled

--- app/src/main/java/com/sbaltazar/pemu_cooking/MainApplication.java
+++ app/src/main/java/com/sbaltazar/pemu_cooking/MainApplication.java
@@ -1,4 +1,16 @@
 package com.sbaltazar.pemu_cooking;
 
-public class MainApplication {
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
 }
